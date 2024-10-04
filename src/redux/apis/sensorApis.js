3d85c053e4b5c7b9df4962116456b0100d4c7457
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-10-04 17:50:26 +0500

    add redux sensor api

---
+++ src/redux/apis/sensorApis.js
@@ -0,0 +1,65 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import getEnv from "../../config/config.js";
+
+const sensorApis = createApi({
+  reducerPath: "sensorApis",
+  baseQuery: fetchBaseQuery({ baseUrl: `${getEnv("SERVER_URL")}/api/sensor`, credentials: "include" }),
+
+  endpoints: (builder) => ({
+    // create sensor
+    // -------------
+    createSensor: builder.mutation({
+      query: (data) => ({
+        url: "/create",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    // get all sensors
+    // ---------------
+    getAllSensors: builder.query({
+      query: () => ({
+        url: "/all",
+        method: "GET",
+      }),
+    }),
+
+    // get single sensors
+    // ------------------
+    getSingleSensor: builder.query({
+      query: (sensorId) => ({
+        url: `/single/${sensorId}`,
+        method: "GET",
+      }),
+    }),
+
+    // update sensor
+    // -------------
+    updateSensor: builder.mutation({
+      query: ({ sensorId, data }) => ({
+        url: `/update/${sensorId}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+
+    // delete sensor
+    // -------------
+    deleteSensor: builder.mutation({
+      query: (sensorId) => ({
+        url: `/single/${sensorId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateSensorMutation,
+  useGetAllSensorsQuery,
+  useGetSingleSensorQuery,
+  useUpdateSensorMutation,
+  useDeleteSensorMutation,
+} = sensorApis;
+export default sensorApis;
