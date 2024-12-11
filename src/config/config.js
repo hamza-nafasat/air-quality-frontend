cb7b8c69d98d5ee846363daec6463eb0476783e8
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-12-11 19:27:37 +0500

    add code

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,10 @@
 const config = Object.freeze({
   SERVER_URL: import.meta.env.VITE_SERVER_URL,
+  API_URL: import.meta.env.VITE_API_URL,
+  COUNTRY_TOKEN: import.meta.env.VITE_GET_COUNTRY_TOKEN,
+  CURRENCY_TOKEN: import.meta.env.VITE_GET_CURRENCY_TOKEN,
+  STRIPE_SECRET_KEY: import.meta.env.VITE_STRIPE_SECRET_KEY,
+  WEATHER_API_KEY: import.meta.env.VITE_WEATHER_API_KEY,
 });
 
 const getEnv = (key) => {
