71ecfd4a66fe728694b447f489591520d87361cc
Ahmed <ahmedcodes48@gmail.com>
2024-12-19 13:01:39 +0500

    autocapitallize off + commit fro ahmed

--- src/components/shared/small/TextField.jsx
+++ src/components/shared/small/TextField.jsx
@@ -3,11 +3,12 @@
 const TextField = ({ label, Icon, ...rest }) => {
   return (
     <div className="flex flex-col gap-1 w-full">
-      {label && <label className="text-sm md:text-base font-[600]">{label}</label>}
+      {label && (
+        <label className="text-sm md:text-base font-[600]">{label}</label>
+      )}
       <section className="w-full border rounded-[10px] border-primary-lightGray px-4 flex items-center gap-3">
         {Icon && Icon}
         <input
-          autoCapitalize="off"
           className="w-full border-none bg outline-none text-sm md:text-base h-[45px] md:h-[50px]"
           {...rest}
         />
