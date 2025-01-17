baee7d0c2cd83ec395bd4cf635a1d26afa8ff244
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-17 18:30:21 +0500

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import sensorApis from "./apis/sensorApis";
 import authSlice from "./slices/authSlice";
 import buildingSlice from "./slices/buildingSlice";
 import floorApis from "./apis/floorApis";
+import subscriptionsApis from "./apis/subscriptionApis";
 
 const store = configureStore({
   reducer: {
@@ -17,13 +18,15 @@ const store = configureStore({
     [buildingApis.reducerPath]: buildingApis.reducer,
     [floorApis.reducerPath]: floorApis.reducer,
     [sensorApis.reducerPath]: sensorApis.reducer,
+    [subscriptionsApis.reducerPath]: subscriptionsApis.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({ serializableCheck: false })
       .concat(authApis.middleware)
       .concat(sensorApis.middleware)
       .concat(buildingApis.middleware)
-      .concat(floorApis.middleware);
+      .concat(floorApis.middleware)
+      .concat(subscriptionsApis.middleware);
   },
 });
 
