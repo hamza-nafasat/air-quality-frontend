134442f8d110c982b7ecd36897dd8948e0108086
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-04-11 17:38:34 +0500

    add

--- src/pages/dashboard/sensors/EditSensor.jsx
+++ src/pages/dashboard/sensors/EditSensor.jsx
@@ -7,26 +7,19 @@ import TextField from "../../../components/shared/small/TextField";
 import { useUpdateSensorMutation } from "../../../redux/apis/sensorApis";
 import { sensorTypes } from "./sensorOptions";
 
-const EditSensor = ({ selectedSensor, onClose, refetch }) => {
+const EditSensor = ({ selectedSensor, onClose }) => {
   const [updateSensor, { isLoading }] = useUpdateSensorMutation("");
   const [form, setForm] = useState({
     name: selectedSensor?.name,
     type: selectedSensor?.type,
     uniqueId: selectedSensor?.uniqueId,
-    url: selectedSensor?.url,
-    location: selectedSensor?.location,
   });
+
   const handleEditSensor = async () => {
     try {
-      if (!form?.name || !form?.type || !form?.uniqueId || !form?.url) {
-        return toast.error("Please fill all the fields");
-      }
-
+      if (!form?.name || !form?.type || !form?.uniqueId) return toast.error("Please fill all the fields");
       const response = await updateSensor({ sensorId: selectedSensor?._id, data: form }).unwrap();
-      if (response?.success) {
-        await refetch();
-        toast.success(response?.message);
-      }
+      if (response?.success) toast.success(response?.message);
     } catch (error) {
       console.log("Error while adding sensor", error);
       toast.error(error?.data?.message || "Error while updating sensor");
@@ -58,22 +51,6 @@ const EditSensor = ({ selectedSensor, onClose, refetch }) => {
           value={form?.uniqueId}
           onChange={(e) => setForm({ ...form, uniqueId: e.target.value })}
         />
-        <TextField
-          label="Location"
-          type="text"
-          placeholder="Device Location"
-          value={form?.location}
-          onChange={(e) => setForm({ ...form, location: e.target.value })}
-        />
-        <div className="col-span-2">
-          <TextField
-            label=" Url"
-            type="text"
-            placeholder="Device Url"
-            value={form?.url}
-            onChange={(e) => setForm({ ...form, url: e.target.value })}
-          />
-        </div>
       </div>
       <div className="flex justify-end mt-4">
         <Button
