d8de093c18895f2f26dad5f68553cc41058aa1aa
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-04-12 19:57:15 +0500

    add

--- src/components/buildingsData/floorEdit/FloorEdit.jsx
+++ src/components/buildingsData/floorEdit/FloorEdit.jsx
@@ -1,96 +1,96 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { useGetAllBuildingsQuery } from '../../../redux/apis/buildingApis';
-import TextField from '../../shared/small/TextField';
-import { useParams } from 'react-router-dom';
-import EditIcon from '../../../assets/svgs/stepper/EditIcon';
-import UploadModelImage from '../../buildings/uploads/UploadModelImage';
-import DeleteIcon from '../../../assets/svgs/pages/DeleteIcon';
-import AccordionEditIcon from '../../../assets/svgs/buildings/AccordionEditIcon';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import AccordionEditIcon from "../../../assets/svgs/buildings/AccordionEditIcon";
+import { useGetSingleFloorQuery, useUpdateSingleFloorMutation } from "../../../redux/apis/floorApis";
+import UploadModelImage from "../../buildings/uploads/UploadModelImage";
+import Button from "../../shared/small/Button";
+import Loader from "../../shared/small/Loader";
+import TextField from "../../shared/small/TextField";
 
 function FloorEdit() {
-    const { id } = useParams();
+  const { id } = useParams();
+  const { data, isLoading } = useGetSingleFloorQuery(id);
+  const [twoDModelPreview, setTwoDModelPreview] = useState(null);
+  const [polygons, setPolygons] = useState([]);
+  const [floor, setFloor] = useState({ floorName: "", floorRooms: "" });
+  const [updateFloor, { isLoading: isUpdatingFloor }] = useUpdateSingleFloorMutation("");
+  const [selectedSensor, setSelectedSensor] = useState([]);
 
-    const { data, isSuccess, isLoading } = useGetAllBuildingsQuery();
-    console.log("data", data)
-    const [twoDModelPreview, setTwoDModelPreview] = useState(null);
-    const [polygons, setPolygons] = useState([]);
-
-    const [floor, setFloor] = useState({
-        floorName: '',
-        floorRooms: '',
-
-    })
-    const formDataHandler = (e) => setBuilding({ ...building, [e.target.name]: e.target.value });
-
-
-    useEffect(() => {
-        if (data?.data) {
-            const building = data?.data?.find((building) =>
-                building?.floors?.some((floor) => floor?._id === id)
-            );
-            const singleFloor = building?.floors?.find((floor) => floor?._id === id);
-            console.log("singleFloor", singleFloor)
-            setFloor({
-                floorName: singleFloor?.name || "",
-                floorRooms: singleFloor?.rooms || "",
-            })
-            setTwoDModelPreview(singleFloor?.twoDModel?.url)
-            setPolygons(JSON.parse(singleFloor?.twoDModelCanvasData))
-        }
-    }, [data, isSuccess, id])
-
-    const deleteButtonHandler = () => {
-
-        console.log('hallo');
+  useEffect(() => {
+    if (data?.data) {
+      const singleFloor = data?.data;
+      setFloor({ floorName: singleFloor?.name || "", floorRooms: singleFloor?.rooms || "" });
+      setTwoDModelPreview(singleFloor?.twoDModel?.url);
+      setPolygons(singleFloor?.twoDModelCanvasData ? JSON.parse(singleFloor?.twoDModelCanvasData) : []);
     }
+  }, [data?.data]);
 
+  const updateFloorHandler = async () => {
+    try {
+      const dataForUpdate = {};
+      if (floor.floorName) dataForUpdate.name = floor.floorName;
+      if (floor.floorRooms) dataForUpdate.rooms = floor.floorRooms;
+      if (polygons) dataForUpdate.twoDModelCanvasData = JSON.stringify(polygons);
+      if (selectedSensor.length > 0) dataForUpdate.sensors = selectedSensor;
+      const res = await updateFloor({ floorId: id, data: dataForUpdate }).unwrap();
+      if (res?.message) toast.success(res.message);
+    } catch (error) {
+      console.log("Error in updating floor", error);
+      toast.error(error?.data?.message || "Error in update floor");
+    }
+  };
 
-    return (
-        <div>
-            <div className="flex items-center justify-between bg-red-400 rounded-[4px] px-4 md:px-8 py-2">
-                <h6 className="text-base md:text-lg font-bold text-white">Floor 1</h6>
-                <div className="flex items-center gap-4">
-                    <div className="cursor-pointer">
-                        <AccordionEditIcon />
-                    </div>
-                </div>
-            </div>
-            <div className='flex gap-4 mt-4'>
-
-                <TextField
-                    type="text"
-                    value={floor.floorName}
-                    placeholder="Floor Name"
-                    onChange={formDataHandler}
-                />
-                <TextField
-                    type="text"
-                    value={floor.floorRooms}
-                    placeholder="Rooms"
-                    onChange={formDataHandler}
-                />
-            </div>
-            <div className="lg:col-span-3 flex justify-center">
-                <div className=''>
-
-                    <div className="flex items-center justify-between my-3 gap-4">
-                        <h3 className="text-sm md:text-base font-semibold text-[rgba(6,6,6,0.8)]">Upload Floor TwoD Model</h3>
-                        <div className="cursor-pointer" onClick={deleteButtonHandler}>
-                            <DeleteIcon />
-                        </div>
-                    </div>
-                    <UploadModelImage
-                        previewValue={twoDModelPreview}
-                        setPreviewValue={setTwoDModelPreview}
-                        polygons={polygons}
-                        setPolygons={setPolygons}
-                    />
-                </div>
-            </div>
-
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div>
+      <div className="flex items-center justify-between bg-[#03A5E0] rounded-[4px] px-4 md:px-8 py-2">
+        <h6 className="text-base md:text-lg font-bold text-white">Floor 1</h6>
+        <div className="flex items-center gap-4">
+          <div className="cursor-pointer">
+            <AccordionEditIcon />
+          </div>
         </div>
-    )
+      </div>
+      <div className="flex gap-4 mt-4">
+        <TextField
+          label={"Floor Name"}
+          type="text"
+          value={floor.floorName}
+          placeholder="Floor Name"
+          onChange={(e) => setFloor({ ...floor, floorName: e.target.value })}
+        />
+        <TextField
+          label={"Rooms"}
+          type="text"
+          value={floor.floorRooms}
+          placeholder="Rooms"
+          onChange={(e) => setFloor({ ...floor, floorRooms: e.target.value })}
+        />
+      </div>
+      <div className="lg:col-span-3 flex justify-center">
+        <div className="mt-4">
+          <UploadModelImage
+            selectedSensor={selectedSensor}
+            setSelectedSensor={setSelectedSensor}
+            previewValue={twoDModelPreview}
+            setPreviewValue={setTwoDModelPreview}
+            polygons={polygons}
+            setPolygons={setPolygons}
+          />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button
+          disabled={isUpdatingFloor}
+          text={"Update Floor"}
+          onClick={updateFloorHandler}
+          className={`max-w-[200px] ${isUpdatingFloor ? "pointer-events-none opacity-30" : ""}`}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default FloorEdit
+export default FloorEdit;
