a5ff9a0076a4b0b1519329ceb130e9a0dd302ce3
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-04-21 19:23:06 +0500

    fix things and add sql new models and use data in floor view

--- src/components/charts/areaChart/DoubleAreaChart.jsx
+++ src/components/charts/areaChart/DoubleAreaChart.jsx
@@ -1,125 +1,79 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 import ChevronIcon from "../../../assets/svgs/buildings/ChevronIcon";
 
-const DoubleAreaChart = () => {
-  const [series, setSeries] = useState([
-    {
-      name: "Temperature",
-      data: [30, 40, 35, 50, 49, 60, 70, 70, 68, 65, 60, 55, 40, 70, 60],
-    },
-    {
-      name: "CO Levels",
-      data: [20, 30, 25, 40, 39, 50, 60, 55, 60, 65, 70, 75, 65, 55, 45],
-    },
-  ]);
-
-  const [options, setOptions] = useState({
+const DoubleAreaChart = ({ chartsData }) => {
+  const [selectedOption, setSelectedOption] = useState("Temp & Humi");
+  const [series, setSeries] = useState([]);
+  const [options] = useState({
     chart: {
       height: 350,
       type: "area",
-      toolbar: {
-        show: false,
-      },
+      toolbar: { show: false },
     },
     colors: ["#33B5E5", "#8E44AD"],
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-      width: 2,
-    },
+    dataLabels: { enabled: false },
+    stroke: { curve: "smooth", width: 2 },
     fill: {
       type: "gradient",
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.4,
-        opacityTo: 0,
-        stops: [0, 90, 100],
-      },
+      gradient: { shadeIntensity: 1, opacityFrom: 0.4, opacityTo: 0, stops: [0, 90, 100] },
     },
     xaxis: {
-      categories: [
-        "01",
-        "02",
-        "03",
-        "04",
-        "05",
-        "06",
-        "07",
-        "08",
-        "09",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14",
-        "15",
-      ],
-      labels: {
-        style: {
-          colors: "#9aa0ac",
-        },
-      },
+      categories: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+      labels: { style: { colors: "#9aa0ac" } },
     },
     yaxis: {
-      min: 10,
-      max: 80,
       labels: {
-        style: {
-          colors: "#9aa0ac",
+        formatter: function (value) {
+          return Number(value).toFixed(0);
         },
       },
     },
-    grid: {
-      show: true,
-      borderColor: "#f1f1f1",
-    },
+    grid: { show: true, borderColor: "#f1f1f1" },
     tooltip: {
       theme: "light",
-      x: {
-        format: "dd/MM",
-      },
-      marker: {
-        show: true,
-      },
-      y: {
-        formatter: (value) => `${value} C`,
-      },
+      x: { format: "dd/MM" },
+      marker: { show: true },
+      y: { formatter: (value) => `${value} C` },
     },
     legend: {
       show: true,
       position: "top",
       horizontalAlign: "center",
-      markers: {
-        fillColors: ["#33B5E5", "#8E44AD"],
-      },
+      markers: { fillColors: ["#33B5E5", "#8E44AD"] },
     },
   });
+  useEffect(() => {
+    if (selectedOption === "Temp & Humi") {
+      setSeries([
+        { name: "temperature", data: chartsData?.temperature },
+        { name: "Humidity", data: chartsData?.humidity },
+      ]);
+    } else if (selectedOption === "Co & Co2") {
+      setSeries([
+        { name: "Co", data: chartsData?.co },
+        { name: "Co2", data: chartsData?.co2 },
+      ]);
+    } else if (selectedOption === "Tvoc & Ch") {
+      setSeries([
+        { name: "Tvoc", data: chartsData?.tvoc },
+        { name: "Ch", data: chartsData?.ch },
+      ]);
+    }
+  }, [selectedOption, chartsData]);
 
   return (
     <div>
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4">
         <h5>Sensors Activity</h5>
-        {/* Example dropdowns for filters */}
         <div className="flex items-center justify-between gap-2">
-          <CustomDropDown lists={["All Floors", "Floor 1", "Floor 2"]} />
-          <CustomDropDown lists={["Temp,CO", "Temp Only", "CO Only"]} />
+          <CustomDropDown
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            lists={["Temp & Humi", "Co & Co2", "Tvoc & Ch"]}
+          />
         </div>
-        {/* <div className="flex gap-2">
-          <select className="border border-gray-300 rounded px-2 py-1">
-            <option>All Floors</option>
-            <option>Floor 1</option>
-            <option>Floor 2</option>
-          </select>
-          <select className="border border-gray-300 rounded px-2 py-1">
-            <option>Temp,CO</option>
-            <option>Temp Only</option>
-            <option>CO Only</option>
-          </select>
-        </div> */}
       </div>
       <ReactApexChart options={options} series={series} type="area" height={350} />
     </div>
@@ -128,9 +82,8 @@ const DoubleAreaChart = () => {
 
 export default DoubleAreaChart;
 
-const CustomDropDown = ({ lists }) => {
+const CustomDropDown = ({ lists, selectedOption, setSelectedOption }) => {
   const [isOptionOpen, setIsOptionOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(lists[0]);
   const selectHandler = (option) => {
     setSelectedOption(option);
     setIsOptionOpen(false);
@@ -139,7 +92,8 @@ const CustomDropDown = ({ lists }) => {
   return (
     <div className="relative z-50">
       <div
-        className="flex items-center justify-between text-sm text-[#060606cc] font-semibold gap-3 cursor-pointer border border-[#060606cc] py-1 px-2 rounded-[8px] text-nowrap"
+        className="flex items-center justify-between text-sm text-[#060606cc] font-semibold gap-3 cursor-pointer 
+        border border-[#060606cc] py-1 px-2 rounded-[8px] text-nowrap"
         onClick={() => optionsHandler()}
       >
         {selectedOption}
