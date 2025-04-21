e508db71c8db4d06efaccb19fc7081e0e102186d
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-04-21 19:22:50 +0500

    add

--- src/pages/dashboard/sensors/sensorOptions.js
+++ src/pages/dashboard/sensors/sensorOptions.js
@@ -25,4 +25,13 @@ const sensorTypes = [
   },
 ];
 
-export { sensorTypes };
+const sensorOptionsForMultiSelect = [
+  { value: "temperature", label: "Temperature" },
+  { value: "humidity", label: "Humidity" },
+  { value: "co", label: "Co" },
+  { value: "co2", label: "Co2" },
+  { value: "ch", label: "Ch" },
+  { value: "tvoc", label: "Tvoc" },
+];
+
+export { sensorTypes, sensorOptionsForMultiSelect };
