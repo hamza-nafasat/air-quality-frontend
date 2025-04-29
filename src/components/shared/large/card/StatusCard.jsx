28f04de782d3d55497ffa29db69c63336279661b
hamza_nafasat <hamzanafasat4155@gmail.com>
2025-04-29 16:19:48 +0500

    add

--- src/components/shared/large/card/StatusCard.jsx
+++ src/components/shared/large/card/StatusCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-const StatusCard = ({ icon, type, status, from, progressIcon, data, smallerName }) => {
+const StatusCard = ({ data, icon, type, status, from, progressIcon, smallerName }) => {
   const SingleData = data?.[smallerName];
   status = SingleData?.level;
   console.log("SingleData", SingleData);
@@ -46,6 +46,28 @@ export const FloorStatusCard = ({ icon, name, value, progressIcon }) => {
             <div>
               <h5 className="text-[12px] xl:text-[14px]">{name?.toUpperCase()}</h5>
               <h3 className="text-[20px] xl:text-[24px]">{value?.level}</h3>
+            </div>
+          </div>
+        </div>
+        {progressIcon && <div className="w-[40%]">{progressIcon}</div>}
+      </div>
+    </div>
+  );
+};
+
+export const BuildingStatusCard = ({ icon, name, value, progressIcon }) => {
+  return (
+    <div
+      className="min-w-[182px] p-4 bg-[white] text-black rounded-[16px] flex flex-col justify-center min-h-[131px] 
+      transition duration-300 ease-in-out shadow-md hover:shadow-md hover:shadow-black/30"
+    >
+      <div className={`flex ${progressIcon && "items-center justify-between gap-2"}`}>
+        <div>
+          <div className="flex items-center gap-3">
+            <div>{icon}</div>
+            <div>
+              <h5 className="text-[12px] xl:text-[14px]">{name?.toUpperCase()}</h5>
+              <h3 className="text-[20px] xl:text-[24px]">{value}</h3>
             </div>
           </div>
         </div>
